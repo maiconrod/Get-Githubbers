5084a339621c0698eeb88b116bbb32333b0a378d
Maicon Rodrigues <maicon_passos@hotmail.com>
2024-05-01 09:55:40 -0300

    Correção de importação não utilizada

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
 import { UserData } from "../types/user";
 import Search from "../components/Search";
-import React from "react";
-
 import { useState } from "react";
 import User from "../components/User";
 import Error from "../components/Error";
